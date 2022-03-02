0bd7015b7764e3a3aeadc714025d26205db59d37
joecoxstrong <joecoxstrong@gmail.com>
2022-03-02 16:11:01 -0500

    Cleaned up main.py. Rearranged methods in alarm_clock.py to follow the flow of the instructions better.

--- alarm_clock.py
+++ alarm_clock.py
@@ -13,7 +13,7 @@ class AlarmClock:
 
     def set_alarm_time(self):
         self.alarm_time = input('Set alarm for: ')
-        print(f'The alarm is set for {self.alarm_time}')
+        # print(f'The alarm is set for {self.alarm_time}')
 
     def turn_alarm_on_or_off(self):
         self.is_alarm_on_or_off = input('Turn the alarm on or off? ')
