c5e9d8ed7aa370d568ac6585d94e7fddbc0f9436
joecoxstrong <joecoxstrong@gmail.com>
2022-03-02 16:02:05 -0500

    Added print current/set time to the console to the alarm_clock.py file. Commented out print operators from the main.py file.

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ alarm_clock.set_time()
 alarm_clock.set_alarm_time()
 alarm_clock.turn_alarm_on_or_off()
 
+print(alarm_clock.current_time)
+# print(alarm_clock.alarm_time)
+# print(alarm_clock.is_alarm_on_or_off)
 
 
